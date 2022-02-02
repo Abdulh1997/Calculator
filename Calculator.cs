f3bb42c85398556461fd8cd88359a18277d5fa96
Abdul1997 <201711212@post.au.dk>
2022-02-02 23:19:00 +0100

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hand_testing_class_Calculator
+{
+    public class Calculator
+    {
+
+
+        public double calc { get; set; }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hand_testing_class_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
